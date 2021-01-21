3d538324c7473718599f4ceea820d403e9ca402c
MURAT ÇOBAN <murcoban@gmail.com>
2021-01-21 17:37:05 +0300

    Proje dosyası ekle.

---
+++ Ocak20Kurs/ProductManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ocak20Kurs
+{
+    class ProductManager
+    {
+    }
+}
---
+++ Ocak20Kurs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ocak20Kurs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
